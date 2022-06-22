c38cfd9393ef4129e281076c98349d3ae8e5b272
dhzdhd <ragas5432100@gmail.com>
2022-06-22 14:43:18 +0530

    Reduce settings dropdown width

--- lib/src/settings/widgets/dropdown_button.dart
+++ lib/src/settings/widgets/dropdown_button.dart
@@ -1,4 +1,3 @@
-import 'package:excode/src/settings/providers/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -10,7 +9,7 @@ class StyledDropdownContainer extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return SizedBox(
-      width: 200,
+      width: 160,
       child: Padding(
         padding: const EdgeInsets.only(left: 8, right: 8),
         child: child,
