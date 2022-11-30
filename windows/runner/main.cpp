be3af4e1ba9329c71757ad1b4e9769cf9166dc5d
dhzdhd <ragas5432100@gmail.com>
2022-11-30 13:48:00 +0530

    Fix android and windows app names

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -32,7 +32,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.CreateAndShow(L"code_executor", origin, size))
+  if (!window.CreateAndShow(L"ExCode", origin, size))
   {
     return EXIT_FAILURE;
   }
